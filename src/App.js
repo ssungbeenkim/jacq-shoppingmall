869c226f872b87b4f4627bc4dbaf2acf55c9a122
wandakim <nwd1583@naver.com>
2023-05-20 02:40:27 +0900

    Set Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Outlet } from 'react-router-dom';
+import Navbar from './components/Navbar';
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import NotFound from './page/NotFound';
+import Home from './page/Home';
+import NewProduct from './page/NewProduct';
+import MyCart from './page/MyCart';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import AllProducts from './page/AllProducts';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: 'products', element: <AllProducts /> },
+      { path: 'products/new', element: <NewProduct /> },
+      { path: 'carts', element: <MyCart /> },
+    ],
+  },
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
