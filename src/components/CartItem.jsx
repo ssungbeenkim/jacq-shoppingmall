248dd48c83037a9593b35bd8badc49667c698213
wandakim <nwd1583@naver.com>
2023-05-24 03:29:23 +0900

    My cart: UI

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { AiOutlineMinusCircle, AiOutlinePlusSquare } from 'react-icons/ai';
+import { AiOutlineMinusSquare, AiOutlinePlusSquare } from 'react-icons/ai';
 import { RiDeleteBin5Fill } from 'react-icons/ri';
 import { addOrUpdateToCart, removeFromCart } from '../api/firebase';
+
+const ICON_CLASS =
+  'transition-all cursor-pointer hover:text-brand hover:scale-105 mx-1';
 
 export default function CartItem({
   product,
@@ -16,16 +19,19 @@ export default function CartItem({
     addOrUpdateToCart(uid, { ...product, quantity: quantity + 1 });
   const handleDelete = () => removeFromCart(uid, id);
   return (
-    <li>
-      <img src={image} alt={title} />
-      <div>
-        <p>{title}</p>
-        <p>{option}</p>
-        <div>
-          <AiOutlineMinusCircle onClick={handleMinus} />
+    <li className='flex justify-between my-2 items-center'>
+      <img className='w-24 md:w-48 rounded-lg' src={image} alt={title} />
+      <div className='flex-1 flex justify-between ml-4'>
+        <div className='basis-3/5'>
+          <p className='text-lg'>{title}</p>
+          <p className='text-xl font-bold text-brand'>{option}</p>
+          <p>€ {price}</p>
+        </div>
+        <div className='text-2xl flex items-center'>
+          <AiOutlineMinusSquare className={ICON_CLASS} onClick={handleMinus} />
           <span>{quantity}</span>
-          <AiOutlinePlusSquare onClick={handlePlus} />
-          <RiDeleteBin5Fill onClick={handleDelete} />
+          <AiOutlinePlusSquare className={ICON_CLASS} onClick={handlePlus} />
+          <RiDeleteBin5Fill className={ICON_CLASS} onClick={handleDelete} />
         </div>
       </div>
     </li>
--- src/page/MyCart.jsx
+++ src/page/MyCart.jsx
@@ -6,6 +6,7 @@ import CartItem from '../components/CartItem';
 import PriceCard from '../components/PriceCard';
 import { BsFillPlusCircleFill } from 'react-icons/bs';
 import { FaEquals } from 'react-icons/fa';
+import Button from '../components/ui/Button';
 
 const SHIPPING = 3000;
 export default function MyCart() {
@@ -22,24 +23,27 @@ export default function MyCart() {
       0
     );
   return (
-    <section>
-      <p>My Cart</p>
+    <section className='p-8 flex flex-col'>
+      <p className='text-2xl text-center font-bold pb-4 border-b border-gray-300'>
+        My Cart
+      </p>
       {!hasProducts && <p>Cart is empty</p>}
       {hasProducts && (
         <>
-          <ul>
+          <ul className='border-b border-gray-300 mb-8 p-4 px-8 '>
             {products &&
               products.map((product) => (
                 <CartItem key={product.id} product={product} uid={uid} />
               ))}
           </ul>
-          <div>
+          <div className='flex justify-between items-center mb-4 p-2 md:px-8 lg:px-16'>
             <PriceCard text='상품 총액' prict={totalPrice} />
-            <BsFillPlusCircleFill />
+            <BsFillPlusCircleFill className='shrink-0' />
             <PriceCard text='배송비' price={SHIPPING} />
-            <FaEquals />
+            <FaEquals className='shrink-0' />
             <PriceCard text='총 가격' price={totalPrice + SHIPPING} />
           </div>
+          <Button text='주문하기' />
         </>
       )}
     </section>
